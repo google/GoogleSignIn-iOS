f642f31524c0b7a25c39cfa4f689bee9cfc23c8f
Matthew Mathias <mdmathias@google.com>
2021-10-12 15:52:17 -0700

    Improves user profile image loading

--- Samples/Swift/GSI-Swift-Sample/GSI-Swift-Sample/UserProfileImageLoader.swift
+++ Samples/Swift/GSI-Swift-Sample/GSI-Swift-Sample/UserProfileImageLoader.swift
@@ -21,37 +21,28 @@ import GoogleSignIn
 /// An observable class for loading the current user's profile image.
 final class UserProfileImageLoader: ObservableObject {
   private let userProfile: GIDProfileData
-
-  /// A `PassthroughSubject` that is called when the `imageData` is received.
-  var didChange = PassthroughSubject<Data, Never>()
   private let imageLoaderQueue = DispatchQueue(label: "com.google.gsi-swift-sample")
-
-  /// The requested image's `Data`.
-  /// - note: The result is lazily loaded and will call `didChange` after it is set.
-  lazy var imageData: Data? = Data() {
-    didSet {
-      guard let imageData = imageData else { return }
-      didChange.send(imageData)
-    }
-  }
+  /// A `UIImage` property containing the current user's profile image.
+  /// - note: This will default to a placeholder, and updates will be published to subscribers.
+  @Published var image = UIImage(named: "PlaceholderAvatar")!
 
   /// Creates an instance of this loader with provided user profile.
   /// - note: The instance will asynchronously fetch the image data upon creation.
   init(userProfile: GIDProfileData) {
     self.userProfile = userProfile
     guard userProfile.hasImage else {
-      self.imageData = UIImage(named: "PlaceholderAvatar")?.pngData()
       return
     }
 
     imageLoaderQueue.async {
       let dimension = 45 * UIScreen.main.scale
       guard let url = userProfile.imageURL(withDimension: UInt(dimension)),
-            let data = try? Data(contentsOf: url) else {
+            let data = try? Data(contentsOf: url),
+            let image = UIImage(data: data) else {
         return
       }
       DispatchQueue.main.async {
-        self.imageData = data
+        self.image = image
       }
     }
   }
--- Samples/Swift/GSI-Swift-Sample/GSI-Swift-Sample/UserProfileImageView.swift
+++ Samples/Swift/GSI-Swift-Sample/GSI-Swift-Sample/UserProfileImageView.swift
@@ -19,22 +19,18 @@ import SwiftUI
 
 struct UserProfileImageView: View {
   @ObservedObject var userProfileImageLoader: UserProfileImageLoader
-  @State var image = UIImage()
 
   init(userProfile: GIDProfileData) {
     self.userProfileImageLoader = UserProfileImageLoader(userProfile: userProfile)
   }
 
   var body: some View {
-    Image(uiImage: image)
+    Image(uiImage: userProfileImageLoader.image)
       .resizable()
       .aspectRatio(contentMode: .fill)
       .frame(width: 45, height: 45, alignment: .center)
       .scaledToFit()
       .clipShape(Circle())
-      .onReceive(userProfileImageLoader.didChange) { imageData in
-        self.image = UIImage(data: imageData) ?? UIImage()
-      }
       .accessibility(hint: Text("Logged in user profile image."))
   }
 }
