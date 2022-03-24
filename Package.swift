cbb864cf58e577b9fc3e868f9c75b623537d3022
Peter Andrews <petea@google.com>
2022-03-23 17:41:58 -0700

    Bump SPM min AppAuth to 1.5

--- Package.swift
+++ Package.swift
@@ -36,7 +36,7 @@ let package = Package(
     .package(
       name: "AppAuth",
       url: "https://github.com/openid/AppAuth-iOS.git",
-      "1.4.0" ..< "2.0.0"),
+      "1.5.0" ..< "2.0.0"),
     .package(
       name: "GTMAppAuth",
       url: "https://github.com/google/GTMAppAuth.git",
