f86649312296d20b17ef2f88bfa263d26214b3d1
mdmathias <mdmathias@google.com>
2022-05-05 10:27:52 -0700

    Adds segmented controls to sample to preview button styling (#126)
    * Adds segmented controls to sample to preview button styling
    
    Fixes 125

--- Samples/Swift/DaysUntilBirthday/Shared/Views/SignInView.swift
+++ Samples/Swift/DaysUntilBirthday/Shared/Views/SignInView.swift
@@ -19,13 +19,54 @@ import GoogleSignInSwift
 
 struct SignInView: View {
   @EnvironmentObject var authViewModel: AuthenticationViewModel
+  @ObservedObject var vm = GoogleSignInButtonViewModel()
 
   var body: some View {
     VStack {
       HStack {
-        GoogleSignInButton(action: authViewModel.signIn)
-          .accessibility(hint: Text("Sign in with Google button."))
-          .padding()
+        VStack {
+          GoogleSignInButton(viewModel: vm, action: authViewModel.signIn)
+            .accessibility(hint: Text("Sign in with Google button."))
+            .padding()
+          VStack {
+            HStack {
+              Text("Button style:")
+                .padding(.leading)
+              Picker("", selection: $vm.style) {
+                ForEach(GoogleSignInButtonStyle.allCases) { style in
+                  Text(style.rawValue.capitalized)
+                    .tag(GoogleSignInButtonStyle(rawValue: style.rawValue)!)
+                }
+              }
+              Spacer()
+            }
+            HStack {
+              Text("Button color:")
+                .padding(.leading)
+              Picker("", selection: $vm.scheme) {
+                ForEach(GoogleSignInButtonColorScheme.allCases) { scheme in
+                  Text(scheme.rawValue.capitalized)
+                    .tag(GoogleSignInButtonColorScheme(rawValue: scheme.rawValue)!)
+                }
+              }
+              Spacer()
+            }
+            HStack {
+              Text("Button state:")
+                .padding(.leading)
+              Picker("", selection: $vm.state) {
+                ForEach(GoogleSignInButtonState.allCases) { state in
+                  Text(state.rawValue.capitalized)
+                    .tag(GoogleSignInButtonState(rawValue: state.rawValue)!)
+                }
+              }
+              Spacer()
+            }
+          }
+          #if os(iOS)
+            .pickerStyle(.automatic)
+          #endif
+        }
       }
       Spacer()
     }
