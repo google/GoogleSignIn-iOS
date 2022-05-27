fa8fbf9530780aa6450a9098b27810e4a1647a3e
Peter Andrews <petea@google.com>
2022-05-27 16:30:04 -0700

    Prevent a build error on Xcode 12 and earlier (#158)
    * Prevent build error on Xcode 12 and earlier.
    
    * Condense.
    
    * Update test as well.

--- GoogleSignIn/Sources/GIDEMMErrorHandler.m
+++ GoogleSignIn/Sources/GIDEMMErrorHandler.m
@@ -148,6 +148,7 @@ typedef enum {
 
 // This method is exposed to the unit test.
 - (nullable UIWindow *)keyWindow {
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 150000
   if (@available(iOS 15, *)) {
     for (UIScene *scene in UIApplication.sharedApplication.connectedScenes) {
       if ([scene isKindOfClass:[UIWindowScene class]] &&
@@ -155,7 +156,9 @@ typedef enum {
         return ((UIWindowScene *)scene).keyWindow;
       }
     }
-  } else {
+  } else
+#endif  // __IPHONE_OS_VERSION_MAX_ALLOWED >= 150000
+  {
 #if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_15_0
     if (@available(iOS 13, *)) {
       for (UIWindow *window in UIApplication.sharedApplication.windows) {
