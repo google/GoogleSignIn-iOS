0cb046296af150d24a4d053d9cde0765890cc44a
Peter Friese <peter@peterfriese.de>
2022-07-17 20:30:28 +0200

    Make asynchronous methods callable via async/await
    This fixes #91

--- GoogleSignIn/Sources/Public/GoogleSignIn/GIDSignIn.h
+++ GoogleSignIn/Sources/Public/GoogleSignIn/GIDSignIn.h
@@ -122,6 +122,7 @@ typedef void (^GIDDisconnectCallback)(NSError *_Nullable error);
 - (void)signInWithConfiguration:(GIDConfiguration *)configuration
        presentingViewController:(UIViewController *)presentingViewController
                        callback:(nullable GIDSignInCallback)callback
+    __attribute__((swift_async_name("signIn(with:presenting:)")))
     NS_EXTENSION_UNAVAILABLE("The sign-in flow is not supported in App Extensions.");
 
 /// Starts an interactive sign-in flow  on iOS using the provided configuration and a login hint.
@@ -143,6 +144,7 @@ typedef void (^GIDDisconnectCallback)(NSError *_Nullable error);
        presentingViewController:(UIViewController *)presentingViewController
                            hint:(nullable NSString *)hint
                        callback:(nullable GIDSignInCallback)callback
+    __attribute__((swift_async_name("signIn(with:presenting:hint:)")))
     NS_EXTENSION_UNAVAILABLE("The sign-in flow is not supported in App Extensions.");
 
 /// Starts an interactive sign-in flow on iOS using the provided configuration and a login hint.
@@ -165,7 +167,8 @@ typedef void (^GIDDisconnectCallback)(NSError *_Nullable error);
        presentingViewController:(UIViewController *)presentingViewController
                            hint:(nullable NSString *)hint
                additionalScopes:(nullable NSArray<NSString *> *)additionalScopes
-                       callback:(nullable GIDSignInCallback)callback;
+                       callback:(nullable GIDSignInCallback)callback
+    __attribute__((swift_async_name("signIn(with:presenting:hint:additionalScopes:)")));
 
 /// Starts an interactive consent flow on iOS to add scopes to the current user's grants.
 ///
@@ -181,6 +184,7 @@ typedef void (^GIDDisconnectCallback)(NSError *_Nullable error);
 - (void)addScopes:(NSArray<NSString *> *)scopes
     presentingViewController:(UIViewController *)presentingViewController
                     callback:(nullable GIDSignInCallback)callback
+    __attribute__((swift_async_name("addScopes(_:presenting:)")))
     NS_EXTENSION_UNAVAILABLE("The add scopes flow is not supported in App Extensions."); 
 
 #elif TARGET_OS_OSX
@@ -198,6 +202,7 @@ typedef void (^GIDDisconnectCallback)(NSError *_Nullable error);
 - (void)signInWithConfiguration:(GIDConfiguration *)configuration
                presentingWindow:(NSWindow *)presentingWindow
                        callback:(nullable GIDSignInCallback)callback;
+    __attribute__((swift_async_name("signIn(with:presenting:)")));
 
 /// Starts an interactive sign-in flow on macOS using the provided configuration and a login hint.
 ///
@@ -215,7 +220,8 @@ typedef void (^GIDDisconnectCallback)(NSError *_Nullable error);
 - (void)signInWithConfiguration:(GIDConfiguration *)configuration
                presentingWindow:(NSWindow *)presentingWindow
                            hint:(nullable NSString *)hint
-                       callback:(nullable GIDSignInCallback)callback;
+                       callback:(nullable GIDSignInCallback)callback
+    __attribute__((swift_async_name("signIn(with:presenting:hint)")));
 
 /// Starts an interactive sign-in flow on macOS using the provided configuration and a login hint.
 ///
@@ -236,7 +242,8 @@ typedef void (^GIDDisconnectCallback)(NSError *_Nullable error);
                presentingWindow:(NSWindow *)presentingWindow
                            hint:(nullable NSString *)hint
                additionalScopes:(nullable NSArray<NSString *> *)additionalScopes
-                       callback:(nullable GIDSignInCallback)callback;
+                       callback:(nullable GIDSignInCallback)callback
+    __attribute__((swift_async_name("signIn(with:presenting:hint:additionalScopes:)")));
 
 /// Starts an interactive consent flow on macOS to add scopes to the current user's grants.
 ///
@@ -249,7 +256,8 @@ typedef void (^GIDDisconnectCallback)(NSError *_Nullable error);
 ///     called asynchronously on the main queue.
 - (void)addScopes:(NSArray<NSString *> *)scopes
        presentingWindow:(NSWindow *)presentingWindow
-               callback:(nullable GIDSignInCallback)callback;
+               callback:(nullable GIDSignInCallback)callback
+    __attribute__((swift_async_name("addScopes(_:presenting:)")));
 
 #endif
 
