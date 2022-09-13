91bc531d641cb0e4934d9c2ec6fc1b72f28d43d4
Peter Andrews <petea@google.com>
2022-09-13 10:04:55 -0700

    Ignore warnings from GTMFetcherAuthorizationProtocol deprecation.

--- GoogleSignIn/Sources/GIDAuthentication.m
+++ GoogleSignIn/Sources/GIDAuthentication.m
@@ -133,9 +133,12 @@ static NSString *const kNewIOSSystemName = @"iOS";
   GTMAppAuthFetcherAuthorizationEMMChainedDelegate *chainedDelegate =
       [[GTMAppAuthFetcherAuthorizationEMMChainedDelegate alloc] initWithDelegate:delegate
                                                                         selector:sel];
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
   [super authorizeRequest:request
                  delegate:chainedDelegate
         didFinishSelector:@selector(authentication:request:finishedWithError:)];
+#pragma clang diagnostic pop
 }
 
 - (void)authorizeRequest:(nullable NSMutableURLRequest *)request
@@ -206,7 +209,10 @@ static NSString *const kNewIOSSystemName = @"iOS";
 
 #pragma mark - Public methods
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 - (id<GTMFetcherAuthorizationProtocol>)fetcherAuthorizer {
+#pragma clang diagnostic pop
 #if TARGET_OS_IOS && !TARGET_OS_MACCATALYST
   GTMAppAuthFetcherAuthorization *authorization = self.emmSupport ?
       [[GTMAppAuthFetcherAuthorizationWithEMMSupport alloc] initWithAuthState:_authState] :
