f4dd91bb6702a85aeddb966e5475b1ff0f3d2908
pinlu <pl1223@nyu.edu>
2022-10-18 14:56:26 -0700

    Update documentation in GIDSignIn_Private.h to make it viewable in Xcode
    Replace `//` with `///`

--- GoogleSignIn/Sources/GIDSignIn_Private.h
+++ GoogleSignIn/Sources/GIDSignIn_Private.h
@@ -29,42 +29,42 @@ NS_ASSUME_NONNULL_BEGIN
 @class GIDGoogleUser;
 @class GIDSignInInternalOptions;
 
-// Represents a completion block that takes a `GIDUserAuth` on success or an error if the operation
-// was unsuccessful.
+/// Represents a completion block that takes a `GIDUserAuth` on success or an error if the operation
+/// was unsuccessful.
 typedef void (^GIDUserAuthCompletion)(GIDUserAuth *_Nullable userAuth, NSError *_Nullable error);
 
 // Private |GIDSignIn| methods that are used internally in this SDK and other Google SDKs.
 @interface GIDSignIn ()
 
-// Redeclare |currentUser| as readwrite for internal use.
+/// Redeclare |currentUser| as readwrite for internal use.
 @property(nonatomic, readwrite, nullable) GIDGoogleUser *currentUser;
 
-// Private initializer for |GIDSignIn|.
+/// Private initializer for |GIDSignIn|.
 - (instancetype)initPrivate;
 
-// Authenticates with extra options.
+/// Authenticates with extra options.
 - (void)signInWithOptions:(GIDSignInInternalOptions *)options;
 
-// Restores a previously authenticated user from the keychain synchronously without refreshing
-// the access token or making a userinfo request. The currentUser.profile will be nil unless
-// the profile data can be extracted from the ID token.
-//
-// @return NO if there is no user restored from the keychain.
+/// Restores a previously authenticated user from the keychain synchronously without refreshing
+/// the access token or making a userinfo request. The currentUser.profile will be nil unless
+/// the profile data can be extracted from the ID token.
+///
+/// @return NO if there is no user restored from the keychain.
 - (BOOL)restorePreviousSignInNoRefresh;
 
 #if TARGET_OS_IOS || TARGET_OS_MACCATALYST
 
-// Starts an interactive consent flow on iOS to add scopes to the current user's grants.
-//
-// The completion will be called at the end of this process.  If successful, a new `GIDGoogleUser`
-// instance will be returned reflecting the new scopes and saved sign-in state will be updated.
-//
-// @param scopes The scopes to ask the user to consent to.
-// @param presentingViewController The view controller used to present `SFSafariViewContoller` on
-//     iOS 9 and 10 and to supply `presentationContextProvider` for `ASWebAuthenticationSession` on
-//     iOS 13+.
-// @param completion The block that is called on completion.  This block will be called asynchronously
-//     on the main queue.
+/// Starts an interactive consent flow on iOS to add scopes to the current user's grants.
+///
+/// The completion will be called at the end of this process.  If successful, a new `GIDGoogleUser`
+/// instance will be returned reflecting the new scopes and saved sign-in state will be updated.
+///
+/// @param scopes The scopes to ask the user to consent to.
+/// @param presentingViewController The view controller used to present `SFSafariViewContoller` on
+///     iOS 9 and 10 and to supply `presentationContextProvider` for `ASWebAuthenticationSession` on
+///     iOS 13+.
+/// @param completion The block that is called on completion.  This block will be called asynchronously
+///     on the main queue.
 - (void)addScopes:(NSArray<NSString *> *)scopes
     presentingViewController:(UIViewController *)presentingViewController
                   completion:(nullable void (^)(GIDUserAuth *_Nullable userAuth,
@@ -73,15 +73,16 @@ typedef void (^GIDUserAuthCompletion)(GIDUserAuth *_Nullable userAuth, NSError *
 
 #elif TARGET_OS_OSX
 
-// Starts an interactive consent flow on macOS to add scopes to the current user's grants
-//
-// The completion will be called at the end of this process.  If successful, a new `GIDGoogleUser`
-// instance will be returned reflecting the new scopes and saved sign-in state will be updated.
-//
-// @param scopes An array of scopes to ask the user to consent to.
-// @param presentingWindow The window used to supply `presentationContextProvider` for `ASWebAuthenticationSession`.
-// @param completion The block that is called on completion.  This block will be called asynchronously
-//     on the main queue.
+/// Starts an interactive consent flow on macOS to add scopes to the current user's grants
+///
+/// The completion will be called at the end of this process.  If successful, a new `GIDGoogleUser`
+/// instance will be returned reflecting the new scopes and saved sign-in state will be updated.
+///
+/// @param scopes An array of scopes to ask the user to consent to.
+/// @param presentingWindow The window used to supply `presentationContextProvider` for
+///     `ASWebAuthenticationSession`.
+/// @param completion The block that is called on completion.  This block will be called asynchronously
+///     on the main queue.
 - (void)addScopes:(NSArray<NSString *> *)scopes
     presentingWindow:(NSWindow *)presentingWindow
           completion:(nullable void (^)(GIDUserAuth *_Nullable userAuth,
