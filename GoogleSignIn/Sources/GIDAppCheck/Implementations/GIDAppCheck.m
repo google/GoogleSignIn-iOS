7a0f2e7ae80053b8429d9dfcff64913e73084103
Matthew Mathias <mdmathias@google.com>
2023-08-25 13:22:44 -0700

    Use debug provider in example app running in DEBUG

--- GoogleSignIn/Sources/GIDAppCheck/Implementations/GIDAppCheck.m
+++ GoogleSignIn/Sources/GIDAppCheck/Implementations/GIDAppCheck.m
@@ -22,6 +22,7 @@
 #import <AppCheckCore/GACAppCheckSettings.h>
 #import <AppCheckCore/GACAppCheckTokenResult.h>
 #import <AppCheckCore/GACAppAttestProvider.h>
+#import <AppCheckCore/GACAppCheckDebugProvider.h>
 
 #import "GoogleSignIn/Sources/GIDAppCheck/Implementations/GIDAppCheck.h"
 #import "GoogleSignIn/Sources/Public/GoogleSignIn/GIDAppCheckError.h"
