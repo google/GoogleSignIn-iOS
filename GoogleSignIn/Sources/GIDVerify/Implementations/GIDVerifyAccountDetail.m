9ffcec1a4d7719539e6a8f2dcf6f708fc9f4102c
brianna <briannamorales@google.com>
2024-03-14 11:09:09 -0700

    Remove spacing in implementation.

--- GoogleSignIn/Sources/GIDVerify/Implementations/GIDVerifyAccountDetail.m
+++ GoogleSignIn/Sources/GIDVerify/Implementations/GIDVerifyAccountDetail.m
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #import "GoogleSignIn/Sources/Public/GoogleSignIn/GIDVerifyAccountDetail.h"
 
 @implementation GIDVerifyAccountDetail
