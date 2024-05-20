25d562415b2aece2131f7317dc715318bfc4ffa3
brianna <briannamorales@google.com>
2024-05-20 14:47:18 -0700

    Add properties and initializer to GIDVerifiedAccountDetailResult class.

--- GoogleSignIn/Sources/Public/GoogleSignIn/GIDVerifiedAccountDetailResult.h
+++ GoogleSignIn/Sources/Public/GoogleSignIn/GIDVerifiedAccountDetailResult.h
@@ -16,7 +16,34 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
+@class GIDVerifiableAccountDetail;
+@class OIDTokenResponse;
+
 /// A helper object that contains the result of a verification flow.
 /// This will pass back the necessary tokens to the requesting party.
 @interface GIDVerifiedAccountDetailResult : NSObject
+
+/// The date when the access token expires.
+@property(nonatomic, readonly, nullable) NSDate *expirationDate;
+/// The access token string.
+@property(nonatomic, copy, readonly) NSString *accessTokenString;
+/// The refresh token string.
+@property(nonatomic, copy, readonly) NSString *refreshTokenString;
+/// A list of verified account details.
+@property(nonatomic, copy, readonly) NSArray<GIDVerifiableAccountDetail *>
+    *verifiedAccountDetails;
+
+/// Initialize a `GIDVerifiedAccountDetailResult` object by specifying all available properties.
+///
+/// @param tokenResponse The last token response with expiration date, access token, and refresh token.
+/// @param accountDetails A list of verified account details.
+///
+/// @return An initialized `GIDVerifyAccountDetail` instance with expiration date, access token, and refresh token.
+- (instancetype)initWithLastTokenResponse:(OIDTokenResponse *)tokenResponse
+                           accountDetails:(NSArray<GIDVerifiableAccountDetail *> *)accountDetails;
+
 @end
+
+NS_ASSUME_NONNULL_END
