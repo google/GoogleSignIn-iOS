3e4ae58e72b43e669d6f3e475e05eb7fccd488e2
Matt Mathias <mdmathias@google.com>
2024-07-12 10:54:51 -0700

    Update swift tools version to 5.5 to satisfy GoogleUtilities

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 // Copyright 2021 Google LLC
