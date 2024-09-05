243cf71aaffa7c02dc41185022587010a0b7477c
brianna <briannamorales@google.com>
2024-09-05 09:29:44 -0700

    Add GIDVerifyAccountDetail import.

--- GoogleSignIn/Sources/GIDSignIn.m
+++ GoogleSignIn/Sources/GIDSignIn.m
@@ -21,6 +21,7 @@
 #import "GoogleSignIn/Sources/Public/GoogleSignIn/GIDProfileData.h"
 #import "GoogleSignIn/Sources/Public/GoogleSignIn/GIDSignInResult.h"
 #import "GoogleSignIn/Sources/Public/GoogleSignIn/GIDVerifiableAccountDetail.h"
+#import "GoogleSignIn/Sources/Public/GoogleSignIn/GIDVerifyAccountDetail.h"
 
 #import "GoogleSignIn/Sources/GIDAuthorizationResponse/GIDAuthorizationResponseHelper.h"
 #import "GoogleSignIn/Sources/GIDAuthorizationResponse/Implementations/GIDAuthorizationResponseHandler.h"
