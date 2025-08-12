f32ad4cc79e28f593a9079eade38c6cd79b2136a
W <115107835+w-goog@users.noreply.github.com>
2025-08-12 16:46:02 -0700

    Mark style methods as preconcurrency

--- GoogleSignInSwift/Sources/GoogleSignInButtonStyling.swift
+++ GoogleSignInSwift/Sources/GoogleSignInButtonStyling.swift
@@ -257,7 +257,7 @@ extension GoogleSignInButtonStyle {
 // MARK: - Button Style
 
 @available(iOS 13.0, macOS 10.15, *)
-struct SwiftUIButtonStyle: ButtonStyle {
+@preconcurrency struct SwiftUIButtonStyle: ButtonStyle {
   let style: GoogleSignInButtonStyle
   let state: GoogleSignInButtonState
   let scheme: GoogleSignInButtonColorScheme
